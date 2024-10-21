099fcf9b911ac67ced3506b8b4f8b27741e96b2c
Dark Thank <dktran004@gmail.com>
2024-10-22 00:23:13 +0700

    Thay doi khach hang

--- src/main/java/ENTITY/KhachHang.java
+++ src/main/java/ENTITY/KhachHang.java
@@ -9,5 +9,32 @@ package ENTITY;
  * @author ADMIN
  */
 public class KhachHang {
+    private int maKH; 
+    private String tenKH; 
+    private String sdt; 
+
+    public int getMaKH() {
+        return maKH;
+    }
+
+    public void setMaKH(int maKH) {
+        this.maKH = maKH;
+    }
+
+    public String getTenKH() {
+        return tenKH;
+    }
+
+    public void setTenKH(String tenKH) {
+        this.tenKH = tenKH;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
     
 }
