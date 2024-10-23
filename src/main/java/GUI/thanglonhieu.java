3df72717967504c50a5d337bb886e60c795a8a7e
Dark Thank <dktran004@gmail.com>
2024-10-23 12:20:36 +0700

    them class get set toString cho thanglonhieu

--- src/main/java/GUI/thanglonhieu.java
+++ src/main/java/GUI/thanglonhieu.java
@@ -9,5 +9,47 @@ package GUI;
  * @author ADMIN
  */
 public class thanglonhieu {
+    private int mathanghieu;
+    private String tenthanghieu; 
+    private String sdt; 
+    private String tenNYC; 
+
+    public int getMathanghieu() {
+        return mathanghieu;
+    }
+
+    public void setMathanghieu(int mathanghieu) {
+        this.mathanghieu = mathanghieu;
+    }
+
+    public String getTenthanghieu() {
+        return tenthanghieu;
+    }
+
+    public void setTenthanghieu(String tenthanghieu) {
+        this.tenthanghieu = tenthanghieu;
+    }
+
+    public String getSdt() {
+        return sdt;
+    }
+
+    public void setSdt(String sdt) {
+        this.sdt = sdt;
+    }
+
+    public String getTenNYC() {
+        return tenNYC;
+    }
+
+    public void setTenNYC(String tenNYC) {
+        this.tenNYC = tenNYC;
+    }
+
+    @Override
+    public String toString() {
+        return "thanglonhieu{" + "mathanghieu=" + mathanghieu + ", tenthanghieu=" + tenthanghieu + ", sdt=" + sdt + ", tenNYC=" + tenNYC + '}';
+    }
+    
     
 }
